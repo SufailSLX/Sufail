3944d16ae98760e5c5e20c9b3985b9059653e55c
SufailSLX <sufailxd@gmail.com>
2025-10-04 22:48:31 +0530

    Header Update 1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-
+import {motion} from 'framer-motion'
+import { a } from 'framer-motion/client'
+import { FiGithub  } from 'react-icons/fi'
 const Header = () => {
   return (
     <header className='absolute w-full z-50 transition-all duration-300'>
         <div className='container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20'>
 
             {/* LOGO NAME  */}
-            <div className='flex items-center'>
+            <motion.div 
+            initial={{ opacity: 0, x: -100}}
+            animate={{ opacity:1, x: 0}}
+            transition={{
+                type:'spring',
+                stiffness:100,
+                damping:25,
+                delay:0.3,
+                duration:1.2,
+            }}
+            className='flex items-center'>
 
                 <div className='h-10 w-10 rounded-xl bg-gradient-to-r from-gray-500 to-gray-100 flex items-center justify-center text-purple-600 font-bold text-xl mr-3'>
                     S
@@ -14,6 +25,37 @@ const Header = () => {
                 <span className='text-xl font-bold bg-gradient-to-r from-gray-600 to-gray-100 bg-clip-text text-transparent'>
                     Sufail
                 </span>
+            </motion.div>
+
+            {/* NAV BAR */}
+            <nav className='lg:flex hidden space-x-8'>
+                    {["Home", "About", "Projects", "Experience", "Contact"].map((item,
+                    index) => (
+                            <motion.a 
+                            initial={{ opacity: 0, y:-20}}
+                            animate={{ opacity:1, y: 0}}
+                            transition={{
+                                type:'string',
+                                stiffness:100,
+                                damping: 20,
+                                delay: 0.7 + index * 0.2,
+                            }}
+                            key={item}
+                            className='relative text-gray-800 dark:text-gray-200 hover:violet-600 dark:hover:text-violet-400 transition-colors duration-300 group'
+                            href="#">
+                                   {item}     
+                                   <span className='absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600
+                                   group-hover:w-full transition-all duration-300 '></span>
+                            </motion.a>
+                    ))}
+            </nav>
+
+            {/* SOCIAL ICONS  */}
+            <div className='md:flex hidden items-center space-x-4'>
+
+                <a href="#">
+                    
+                </a>
             </div>
         </div>
     </header>
