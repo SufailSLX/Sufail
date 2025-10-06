45c3fa49d2f89a8a0c5c1aaae469523c22b52254
SufailSLX <sufailxd@gmail.com>
2025-10-06 16:32:37 +0530

    Cursor Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import CustomCursor from './components/CustomCursor'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 
@@ -7,6 +8,7 @@ const App = () => {
     <>
     <Header />
     <HeroSection />
+    <CustomCursor />
     </>
     
   )
