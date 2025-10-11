393b765bd994396c930e0ec36dc2fc79448ef07c
SufailSLX <sufailxd@gmail.com>
2025-10-11 09:35:12 +0530

    PROJECT -Build 1.1

---
+++ src/components/ProjectSection.jsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react'
+
+const ProjectSection = () => {
+    const sectionRef = useRef(null)
+  return (
+    <section
+    ref={sectionRef}
+    id="horizantol-section"
+    className='realative py-20 bg-[#f6f6f6]'
+    >
+
+      {/* section title  */}
+        
+    </section>
+  )
+}
+
+export default ProjectSection
