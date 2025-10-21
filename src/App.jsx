cc7f7eb6433b26983188e8f912cfb4ce1b3348c1
SufailSLX <sufailxd@gmail.com>
2025-10-21 11:06:45 +0530

    CONTACT 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import AboutSection from './components/AboutSection'
 import CustomCursor from './components/CustomCursor'
 import Header from './components/Header'
@@ -6,6 +10,21 @@ import ProjectSection from './components/ProjectSection'
 
 
 const App = () => {
+
+  useEffect(()=>{
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    //Refresh scroll
+
+    ScrollTrigger.refresh()
+
+    // clean up scroll 
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger)=> trigger.kill ())
+    }
+  },[])
+
   return (
     <>
     <Header />
