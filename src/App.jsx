e2b89eeb4e35a8c4dfa118a1548f442098413fa9
SufailSLX <sufailxd@gmail.com>
2025-10-29 10:15:26 +0530

    Progess update

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
           <>
             <Header />
             <HeroSection />
-            {/* <CustomCursor /> */}
+            <CustomCursor />
             <AboutSection />
             <ProjectSection />
             <ContactSection />
