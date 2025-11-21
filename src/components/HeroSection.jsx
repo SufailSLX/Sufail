8633e43e80582fdb82201d2fdfd3d4dc7657f364
SufailSLX <sufailxd@gmail.com>
2025-11-21 10:21:55 +0530

    BG Changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -48,8 +48,17 @@ const HeroSection = () => {
       </div>
 
       {/* RIGHT SIDE  */}
-
-      <Spline className="absolute xl:right-[-28] right-0 top-[-20%] lg:top-0" scene="https://prod.spline.design/EtskZCF10O0zBMV2/scene.splinecode" />
+            <div className="absolute xl:right-[-28] right-0 top-[-20%] lg:top-0 overflow-hidden">
+  <div style={{
+    width: 'calc(100% + 80px)',
+    height: 'calc(100% + 80px)',
+    marginRight: '-80px',
+    marginBottom: '-80px'
+  }}>
+    <Spline scene="https://prod.spline.design/EtskZCF10O0zBMV2/scene.splinecode" />
+  </div>
+</div>
+      {/* <Spline className="absolute xl:right-[-28] right-0 top-[-20%] lg:top-0" scene="https://prod.spline.design/EtskZCF10O0zBMV2/scene.splinecode" /> */}
     </section>
   );
 };
